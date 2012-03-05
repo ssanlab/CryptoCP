693795b66789e392d690abb57e991aa78d624dca
Sergey S <sshatalin@anlab.ru>
2012-03-05 11:04:17 +0400

    [+|*] Add hash(), signf() public methods

--- CryptoCP.php
+++ CryptoCP.php
@@ -15,11 +15,23 @@ class CryptoCP
      **/
     private $data;
     /**
+     * $signMsg
+     *
+     * @var string
+     **/
+    private $signMsg = null;
+    /**
      * $sign
      *
      * @var string
      **/
     private $sign = null;
+    /**
+     * $hash
+     *
+     * @var string
+     **/
+    private $hash = null;
     /**
      * $fileName
      *
@@ -33,11 +45,24 @@ class CryptoCP
      **/
     private $dataFile = null;
     /**
+     * $signMsgFile
+     *
+     * @var string
+     **/
+    private $signMsgFile = null;
+    /**
      * $signFile
      *
      * @var string
      **/
     private $signFile = null;
+    /**
+     * $hashFile
+     *
+     * @var string
+     **/
+    private $hashFile = null;
+    
     /**
      * __construct
      *
@@ -56,11 +81,11 @@ class CryptoCP
      **/
     public function sign()
     {
-        if ($this->createFile()) {
+        if (is_readable($this->dataFile) || $this->createFile()) {
             $this->signFile();
             if (CLEANUP_AFTER_SIGN) {
                 $this->deleteFile($this->dataFile);
-                $this->deleteFile($this->signFile);
+                $this->deleteFile($this->signMsgFile);
             }
         }
         return $this->getSign();
@@ -74,13 +99,98 @@ class CryptoCP
     private function signFile()
     {
         if (is_readable($this->dataFile)) {
-            $this->signFile = FILES_DIR . '\\' . $this->fileName . '.' 
-                            . SIGN_FILE_EXTENSION;
-            exec(CRYPTOCP_DIR . '\\' . CRYPTOCP_FILENAME . ' -sign -f "' 
-               . CERT_DIR . '\\' . CERT_FILENAME . '" -pin "' . CERT_PASSWORD 
-               . '" "' . $this->dataFile . '" "' . $this->signFile . '"');
+            $this->signMsgFile = SIGN_MSG_DIR . DIRECTORY_SEPARATOR 
+                               . $this->fileName 
+                               . '.' . SIGN_MSG_FILE_EXTENSION;
+            exec(CRYPTCP_DIR . DIRECTORY_SEPARATOR . CRYPTCP_FILENAME 
+               . ' -sign -f "' . CERT_DIR . DIRECTORY_SEPARATOR . CERT_FILENAME
+               . '" -pin "' . CERT_PASSWORD . '" "' . $this->dataFile . '" "' 
+               . $this->signMsgFile . '"');
             if (true) {
-                if ((@$sign = file_get_contents($this->signFile)) === false)
+                if ((@$signMsg = file_get_contents($this->signMsgFile)) 
+                    === false
+                )
+                    return false;
+                $this->signMsg = $signMsg;
+                return true;
+           }
+        }
+    }
+    /**
+     * hash
+     *
+     * @return string
+     * @author Sergey
+     **/
+    public function hash()
+    {
+        if (is_readable($this->dataFile) || $this->createFile()) {
+            $this->hashFile();
+            if (CLEANUP_AFTER_SIGN) {
+                $this->deleteFile($this->dataFile);
+                $this->deleteFile($this->hashFile);
+            }
+        }
+        return $this->getHash();
+    }
+    /**
+     * hashFile
+     *
+     * @return bool
+     * @author Sergey
+     **/
+    private function hashFile()
+    {
+        if (is_readable($this->dataFile)) {
+            $this->hashFile = HASH_DIR . DIRECTORY_SEPARATOR . $this->fileName 
+                      . '.' . DATA_FILE_EXTENSION . '.' . HASH_FILE_EXTENSION;
+            // cryptcp -hash -dir hashes test.txt
+            exec(CRYPTCP_DIR . DIRECTORY_SEPARATOR . CRYPTCP_FILENAME 
+               . ' -hash -dir "' . HASH_DIR . '" "' . $this->dataFile . '"');
+            if (true) {
+                if (($hash = file_get_contents($this->hashFile)) === false)
+                    return false;
+                $this->hash = base64_encode($hash);
+           }
+        }
+    }
+    /**
+     * signf
+     *
+     * @return string
+     * @author Sergey
+     **/
+    public function signf()
+    {
+        if (is_readable($this->dataFile) || $this->createFile()) {
+            $this->signfFile();
+            if (CLEANUP_AFTER_SIGN) {
+                $this->deleteFile($this->dataFile);
+                $this->deleteFile($this->File);
+            }
+        }
+        return $this->getSignf();
+    }
+    /**
+     * hashFile
+     *
+     * @return bool
+     * @author Sergey
+     **/
+    private function signfFile()
+    {
+        if (is_readable($this->dataFile)) {
+            $this->signFile = SIGN_DIR . DIRECTORY_SEPARATOR . $this->fileName 
+                            . '.' . DATA_FILE_EXTENSION 
+                            . '.' . SIGN_FILE_EXTENSION;
+            // cryptcp -signf -dir \signs -f cert.crt d:\*.doc 
+            exec(CRYPTCP_DIR . DIRECTORY_SEPARATOR . CRYPTCP_FILENAME 
+               . ' -signf -dir "' . SIGN_DIR . '" -f "' . CERT_DIR 
+               . DIRECTORY_SEPARATOR . CERT_FILENAME . '" -pin "' 
+               . CERT_PASSWORD . '" "' . $this->dataFile . '" -sd' 
+               . TSP_ADDRESS);
+            if (true) {
+                if (($sign = file_get_contents($this->signFile)) === false)
                     return false;
                 $this->sign = $sign;
            }
@@ -95,8 +205,8 @@ class CryptoCP
     private function createFile()
     {
         $this->fileName = $this->generateFileName();
-        $this->dataFile = FILES_DIR . '\\' . $this->fileName . '.' 
-                        . DATA_FILE_EXTENSION;
+        $this->dataFile = FILE_DIR . DIRECTORY_SEPARATOR . $this->fileName 
+                        . '.' . DATA_FILE_EXTENSION;
         if (file_put_contents($this->dataFile, $this->data) === false)
             return false;
         return true;
@@ -154,6 +264,26 @@ class CryptoCP
      * @author Sergey
      **/
     public function getSign()
+    {
+        return $this->signMsg;
+    }
+    /**
+     * getHash
+     *
+     * @return string
+     * @author Sergey
+     **/
+    public function getHash()
+    {
+        return $this->hash;
+    }
+    /**
+     * getSign
+     *
+     * @return string
+     * @author Sergey
+     **/
+    public function getSignf()
     {
         return $this->sign;
     }
